16c52b9c7bf7ce201f91fdd85f91a6ec8856e4f9
yuhaifei123 <1129639939@qq.com>
2017-03-17 16:52:44 +0800

    x

--- src/com/example/youkudemo/MainActivity.java
+++ src/com/example/youkudemo/MainActivity.java
@@ -13,6 +13,15 @@ public class MainActivity extends Activity {
 		setContentView(R.layout.activity_main);
 		
 		System.out.println("fsdfs");
+		System.out.println("fsdfs");
+		System.out.println("fsdfs");
+		System.out.println("fsdfs");
+		System.out.println("fsdfs");
+		System.out.println("fsdfs");
+		System.out.println("fsdfs");
+		System.out.println("fsdfs");
+		System.out.println("fsdfs");
+		
 	}
 
 	@Override
